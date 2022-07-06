a486c1b67efdbbb64fcd342f8c9f18ecf356e610
Preet Sharma <74368730+ThePreetSharma@users.noreply.github.com>
2022-07-06 20:52:47 +0530

    Merger of blog and main Done

--- JS/scriptForIndex.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementsByClassName('hamburger')[0]
-const navlinks = document.getElementsByClassName('nav-links')[0]
-
-hamburger.addEventListener('click', () => {
-    navlinks.classList.toggle('active')
-})
